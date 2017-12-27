1435b5203e0395d1a756a2135ca876a5743b8914
Narendra Sisodiya <narendra@billexo.com>
2017-12-27 13:32:25 +0530

    added failed test cases.

---
+++ task1/add.js
@@ -0,0 +1,7 @@
+/**
+ * Created by narendrasisodiya on 27/12/17.
+ */
+
+
+module.exports = function add(a, b) {
+};
--- task1/test/test.js
+++ task1/test/test.js
@@ -1,8 +1,11 @@
+var add = require('../add');
+
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
-    });
+describe('Add Test', function() {
+  it('should add integer', function() {
+    assert.equal(add(3, 4), 7);
+  });
+  it('should add strings too', function() {
+    assert.equal(add('3', '4'), 7);
   });
 });
