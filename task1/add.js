43a378f162f205bb206b48127ed35426bd26abe5
Kajal Sharma <34881990+sharmakajal@users.noreply.github.com>
2017-12-28 16:22:55 +0530

    Update add.js
    Updated

--- task1/add.js
+++ task1/add.js
@@ -1,12 +1,12 @@
 /**
  * Created by narendrasisodiya on 27/12/17.
  */
-
-
 module.exports = function add(a, b) {
   
-git clone https://github.com/nsisodiya/intern
-cd ./intern/task1
-npm install
-npm run test
+  int a;
+  int b;
+  
+  b=a+b;
+  return b;
+
 };
